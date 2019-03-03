6d0ae76590650ef3a0f7f2171a2c5d22e3f93487
Yashu Mittal <mittalyashu77@gmail.com>
2019-03-03 21:07:29 +0530

    REVIEW: What if user update this username?
    Add a comment anchor "What if user update this username?"

--- user.js
+++ user.js
@@ -3,6 +3,7 @@ const userSchema = require("./model/user")
 const createUser = async msgData => {
 	const userData = userSchema.build({
 		id: msgData.id,
+		// REVIEW What if user update this username?
 		name: msgData.username,
 	})
 	const result = await userData.save()
